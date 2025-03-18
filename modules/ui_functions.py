d762d4d09410194081fedab7eadee2cc19762b7a
ali7haider <ali7haider.dev@gmail.com>
2025-03-18 12:53:22 +0500

    Remove app_settings.py and refactor UI functions to use hardcoded settings

--- modules/ui_functions.py
+++ modules/ui_functions.py
@@ -1,4 +1,3 @@
-from modules.app_settings import Settings
 from modules.custom_grips import CustomGrip
 from PyQt5.QtGui import QIcon, QColor
 from PyQt5.QtCore import QPropertyAnimation, QEasingCurve, QParallelAnimationGroup, QTimer, Qt
@@ -9,6 +8,8 @@ from main_screen_logic import MasterScreen  # Import the MainApp class from main
 
 GLOBAL_STATE = False
 GLOBAL_TITLE_BAR = True
+ENABLE_CUSTOM_TITLE_BAR=True
+
 class UIFunctions(MasterScreen):
     
     def maximize_restore(self):
@@ -51,133 +52,7 @@ class UIFunctions(MasterScreen):
 
     # TOGGLE MENU
     # ///////////////////////////////////////////////////////////////
-    def toggleMenu(self, enable):
-        if enable:
-            # GET WIDTH
-            width = self.ui.leftMenuBg.width()
-            maxExtend = Settings.MENU_WIDTH
-            standard = 70
-
-            # SET MAX WIDTH
-            if width == 70:
-                widthExtended = maxExtend
-            else:
-                widthExtended = standard
-
-            # ANIMATION
-            self.animation = QPropertyAnimation(self.ui.leftMenuBg, b"minimumWidth")
-            self.animation.setDuration(Settings.TIME_ANIMATION)
-            self.animation.setStartValue(width)
-            self.animation.setEndValue(widthExtended)
-            self.animation.setEasingCurve(QEasingCurve.InOutQuart)
-            self.animation.start()
-
-
-    # TOGGLE LEFT BOX
-    # ///////////////////////////////////////////////////////////////
-    def toggleLeftBox(self, enable):
-        if enable:
-            # GET WIDTH
-            width = self.ui.extraLeftBox.width()
-            widthRightBox = self.ui.extraRightBox.width()
-            maxExtend = Settings.LEFT_BOX_WIDTH
-            color = Settings.BTN_LEFT_BOX_COLOR
-            standard = 0
-
-            # GET BTN STYLE
-            style = self.ui.toggleLeftBox.styleSheet()
-
-            # SET MAX WIDTH
-            if width == 0:
-                widthExtended = maxExtend
-                # SELECT BTN
-                self.ui.toggleLeftBox.setStyleSheet(style + color)
-                if widthRightBox != 0:
-                    style = self.ui.settingsTopBtn.styleSheet()
-                    self.ui.settingsTopBtn.setStyleSheet(style.replace(Settings.BTN_RIGHT_BOX_COLOR, ''))
-            else:
-                widthExtended = standard
-                # RESET BTN
-                self.ui.toggleLeftBox.setStyleSheet(style.replace(color, ''))
-                
-        UIFunctions.start_box_animation(self, width, widthRightBox, "left")
-
-    # TOGGLE RIGHT BOX
-    # ///////////////////////////////////////////////////////////////
-    def toggleRightBox(self, enable):
-        if enable:
-            # GET WIDTH
-            width = self.ui.extraRightBox.width()
-            widthLeftBox = self.ui.extraLeftBox.width()
-            maxExtend = Settings.RIGHT_BOX_WIDTH
-            color = Settings.BTN_RIGHT_BOX_COLOR
-            standard = 0
-
-            # GET BTN STYLE
-            style = self.ui.settingsTopBtn.styleSheet()
-
-            # SET MAX WIDTH
-            if width == 0:
-                widthExtended = maxExtend
-                # SELECT BTN
-                self.ui.settingsTopBtn.setStyleSheet(style + color)
-                if widthLeftBox != 0:
-                    style = self.ui.toggleLeftBox.styleSheet()
-                    self.ui.toggleLeftBox.setStyleSheet(style.replace(Settings.BTN_LEFT_BOX_COLOR, ''))
-            else:
-                widthExtended = standard
-                # RESET BTN
-                self.ui.settingsTopBtn.setStyleSheet(style.replace(color, ''))
-
-            UIFunctions.start_box_animation(self, widthLeftBox, width, "right")
-
-    def start_box_animation(self, left_box_width, right_box_width, direction):
-        right_width = 0
-        left_width = 0 
-
-        # Check values
-        if left_box_width == 0 and direction == "left":
-            left_width = 240
-        else:
-            left_width = 0
-        # Check values
-        if right_box_width == 0 and direction == "right":
-            right_width = 240
-        else:
-            right_width = 0       
-
-        # ANIMATION LEFT BOX        
-        self.left_box = QPropertyAnimation(self.ui.extraLeftBox, b"minimumWidth")
-        self.left_box.setDuration(Settings.TIME_ANIMATION)
-        self.left_box.setStartValue(left_box_width)
-        self.left_box.setEndValue(left_width)
-        self.left_box.setEasingCurve(QEasingCurve.InOutQuart)
-
-        # ANIMATION RIGHT BOX        
-        self.right_box = QPropertyAnimation(self.ui.extraRightBox, b"minimumWidth")
-        self.right_box.setDuration(Settings.TIME_ANIMATION)
-        self.right_box.setStartValue(right_box_width)
-        self.right_box.setEndValue(right_width)
-        self.right_box.setEasingCurve(QEasingCurve.InOutQuart)
-
-        # GROUP ANIMATION
-        self.group = QParallelAnimationGroup()
-        self.group.addAnimation(self.left_box)
-        self.group.addAnimation(self.right_box)
-        self.group.start()
-
-    # SELECT/DESELECT MENU
-    # ///////////////////////////////////////////////////////////////
-    # SELECT
-    def selectMenu(getStyle):
-        select = getStyle + Settings.MENU_SELECTED_STYLESHEET
-        return select
-
-    # DESELECT
-    def deselectMenu(getStyle):
-        deselect = getStyle.replace(Settings.MENU_SELECTED_STYLESHEET, "")
-        return deselect
-
+    
     # START - GUI DEFINITIONS
     # ///////////////////////////////////////////////////////////////
     def uiDefinitions(self):
@@ -186,8 +61,7 @@ class UIFunctions(MasterScreen):
             if event.type() == QEvent.MouseButtonDblClick:
                 QTimer.singleShot(250, lambda: UIFunctions.maximize_restore(self))
         self.ui.leftBox.mouseDoubleClickEvent = dobleClickMaximizeRestore
-
-        if Settings.ENABLE_CUSTOM_TITLE_BAR:
+        if ENABLE_CUSTOM_TITLE_BAR:
             #STANDARD TITLE BAR
             self.setWindowFlags(Qt.FramelessWindowHint)
             self.setAttribute(Qt.WA_TranslucentBackground)
@@ -238,7 +112,7 @@ class UIFunctions(MasterScreen):
         self.ui.closeAppBtn.clicked.connect(lambda: self.close())
 
     def resize_grips(self):
-        if Settings.ENABLE_CUSTOM_TITLE_BAR:
+        if ENABLE_CUSTOM_TITLE_BAR:
             self.left_grip.setGeometry(0, 10, 10, self.height())
             self.right_grip.setGeometry(self.width() - 10, 10, 10, self.height())
             self.top_grip.setGeometry(0, 0, self.width(), 10)
